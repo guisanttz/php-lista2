73d6f9cae78f2e4c3aab6060c45f357dba1c33f7
Gui <guilhermerafaell10@gmail.com>
2026-08-28 14:03:55 -0300

    criação do index

--- ex1.php
+++ ex1.php
@@ -1,11 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html lang="pt-BR">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Estrutura Condicional - If e Switch</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body>
-    
+    <div class="container py-3">
+        <h1>Estrutura Condicional - If e Switch</h1>
+        <form method="post">
+            <div class="mb-3">
+                <label for="numero" class="form-label">Informe o número</label>
+                <input type="text" id="numero" name="numero" class="form-control" required="">
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
+            crossorigin="anonymous"></script>
+    </div>
 </body>
+
 </html>
