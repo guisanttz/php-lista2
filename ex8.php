219c1a8f913fc9de2142c98015f24f6d2a245a9f
Gui <guilhermerafaell10@gmail.com>
2026-09-01 17:11:51 -0300

    exercício 8 finalizado

---
+++ ex8.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercício 8 - Fatorial</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container py-3">
+        <h1>Exercício 8 - Fatorial</h1>
+        <form method="post">
+            <div class="mb-3">
+                <label for="numero" class="form-label">Insira um número: </label>
+                <input type="number" id="numero" name="numero" class="form-control" required="">
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button><br>
+            <a href="index.html">Retornar ao início</a>
+        </form>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
+            crossorigin="anonymous"></script>
+    </div>
+</body>
+
+</html>
+
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+    $numero = $_POST['numero'];
+    $resultado = 1;
+
+    for ($i = $numero; $i > 0; $i--){
+
+        $resultado = $resultado * $i;
+
+    }
+
+    echo "<h4>$resultado</h4>";
+
+}
